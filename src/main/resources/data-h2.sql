f582d7c9855bfd37e5f66880997dcf0a81ff4b0b
surenusi <mixma123@naver.com>
2022-04-26 19:24:14 +0900

    Chore: application.yml 수정 및 data-h2.sql 스크립트 추가 Read테스트를 위한 DB테이블 초기화 작업을 수행하기 위해 application.yml을 수정 및 data-h2.sql 스크립트 추가 data-h2.sql스크립트가 Local환경에서만 돌아가도록 application.yml을 수정했음

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+insert into posts(title, author, content, created_date, modified_date) 
+    values ('test title1', 'test author1', 'test content', now(), now());
+insert into posts(title, author, content, created_date, modified_date) 
+    values ('test title2', 'test author2', 'test content', now(), now());
